b5d5cdc1da2d9a09f8133bc33c998e319fecad42
Danila Sizykh <d_sizykh@mail.ru>
2018-09-06 22:56:34 +0300

    ground for the house now works with zero output & strict mode

--- checkio/ground_for_the_house.js
+++ checkio/ground_for_the_house.js
@@ -1,5 +1,6 @@
 // https://js.checkio.org/mission/ground-for-the-house/
 
+
 function house(plan) {
     if (plan[0] == "\n") plan = plan.slice(1);
     let getFirstHash = p => {for (let i = 0; i < p.length; i++) if (p[i].indexOf("#") != -1) return i};
@@ -8,11 +9,10 @@ function house(plan) {
     plan.reverse();
     plan = plan.slice(getFirstHash(plan));
 
-    let reversedPlan = plan.slice();
+    let reversedPlan = [];
     for (let y = 0; y < plan.length; y++) {
         for (let x = 0, rowLen = plan[0].length; x < rowLen; x++) {
             if (!reversedPlan[x]) reversedPlan[x] = [];
-            console.log(reversedPlan);
             reversedPlan[x][y] = plan[y][x];
         }
     }
@@ -20,5 +20,5 @@ function house(plan) {
     reversedPlan.reverse();
     reversedPlan = reversedPlan.slice(getFirstHash(reversedPlan));
 
-    return reversedPlan.length * reversedPlan[0].length;
+    return reversedPlan.length && reversedPlan.length * reversedPlan[0].length;
 }
