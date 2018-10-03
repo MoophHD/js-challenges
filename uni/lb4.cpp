668788ff03dfe2321e8b029b16d93eab23fdac51
Danila Sizykh <d_sizykh@mail.ru>
2018-10-03 22:10:53 +0300

    lb4

---
+++ uni/lb4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int k, n, i;
+
+    //  cout << "Enter array length: ";
+    //     cin >> k;
+    // cout << "Enter shift value: ";
+    //     cin >> n;
+
+    k = 40;
+    n = 5;
+
+    int arr[k];
+
+    i = 0;
+    while (i < k) {
+        arr[i] = i;
+        i++;
+    }
+
+    
+
+    i = 0;
+    while(i < k {
+        if (i < k - n) {
+            arr[i] = arr[i + n];
+            i++
+        } else {
+
+        }
+    }
+
+    // cout << arr[0];
+
+    return 1;
+}
